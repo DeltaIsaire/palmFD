84feda2243cf15ee2b918f8decf9378f0ca266ae
Sebastiaan Haverhoek <management.organisatie@gmail.com>
2018-11-15 10:43:24 +0100

    subsetted palm traits dataset and renamed README
    changed readme file extension from .txt to .md

--- 1_trait_filling.R
+++ 1_trait_filling.R
@@ -2,13 +2,13 @@
 # Palm FD project: gap-filling trait matrix
 ###########################################
 #
-# In which (...)
+# In which <description>
 # Input files:
 #   data/palms_in_tdwg3.csv
 #   data/PalmTraits_10.csv
 #   data/TREE.nex
 # Generated output files:
-#   none
+#   output/palm.traits.subset.csv
 
 library(ape)
 
@@ -31,7 +31,7 @@ trait.data <- read.csv(file="data/PalmTraits_10.csv")
 #	31. extra reference notes
 
 # Read phylogenetic tree
-tree <- read.nexus(file="data/TREE.nex")
+palm.tree <- read.nexus(file="data/TREE.nex")
 
 
 # Explore if these three sources agree on which palm species exist
@@ -40,20 +40,33 @@ tree <- read.nexus(file="data/TREE.nex")
 species <- list(
   palm.dist  = levels(palm.dist$SpecName), 
   trait.data = sub(pattern=" ", replacement="_", x=trait.data$SpecName), 
-  tree       = tree$tip.label)
-# 2. cross-reference
+  palm.tree  = palm.tree$tip.label)
+# 2. cross-check
 missing.table <- multiCC(species, presence=FALSE, value=FALSE)
 missing.list <- multiCC(species, presence=FALSE, value=TRUE)
-# palm.dist and trait.data are the same, but tree differs from the others.
+# palm.dist and trait.data are the same, but palm.tree differs from the others.
 # 3. Find the list of species upon which all three sources agree
 present.list <- multiCC(species, presence=TRUE, value=TRUE)
 # Just to verify:
 new.species <- list(
-  palm.dist = present.list$palm.dist$tree,
-  tree      = present.list$tree$palm.dist)
+  palm.dist = present.list$palm.dist$palm.tree,
+  palm.tree = present.list$palm.tree$palm.dist)
 multiCC(new.species, presence=FALSE, value=FALSE)
-# Yup, nothing missing
-species.agreed <- sort(present.list$palm.dist$tree)
+# Yup, nothing missing.
+species.agreed <- sort(present.list$palm.dist$palm.tree)
+
+
+# Subset palm traits dataset
+palm.traits    <- data.frame(
+  species      = sub(pattern=" ", replacement="_", x=trait.data$SpecName),
+  genus        = trait.data$accGenus,
+  stem.height  = trait.data$MaxStemHeight_m,  # maximum stem height in m.
+  blade.length = trait.data$Max_Blade_Length_m,  # maximum blade length in m.
+  fruit.length = trait.data$AverageFruitLength_cm)  # Average fruit length in cm.
+palm.traits <- palm.traits[crossCheck(species.agreed,
+  palm.traits$species, presence=TRUE, value=FALSE), ]
+write.csv(palm.traits, file="output/palm.traits.subset.csv", eol="\r\n")
+
 
 
 # TODO: Subset all datasets to the list of agreed species.
