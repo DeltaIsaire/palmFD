96cebc225022d3e1d86255c4ddf18d33941f3452
Sebastiaan Haverhoek <management.organisatie@gmail.com>
2018-11-16 16:04:15 +0100

    Improved trait-filling with genus means:
    In a few cases, all species within a genus had NA values for the same trait,
    making it impossible to calculate a genus mean.
    These cases are removed from the genusMean-filled trait matrix.
    The deleted cases are recorded in output/genus.mean.missing.csv

--- 1_trait_filling.R
+++ 1_trait_filling.R
@@ -8,10 +8,12 @@
 #   data/PalmTraits_10.csv
 #   data/TREE.nex
 # Generated output files:
-#   output/palm.traits.subset.csv
+#   output/palm.traits.csv
+#   output/palm.traits.genus.mean.csv
 
 library(ape)
 library(plyr)
+
 
 # Read palm distribution data
 palm.dist <- read.csv(file="data/palms_in_tdwg3.csv")
@@ -38,10 +40,10 @@ palm.tree <- read.nexus(file="data/TREE.nex")
 # Explore if these three sources agree on which palm species exist
 # 1. Extract species names
 # Ensure that each variable uses underscores instead of spaces
-species <- list(
-  palm.dist  = levels(palm.dist$SpecName), 
-  trait.data = sub(pattern=" ", replacement="_", x=trait.data$SpecName), 
-  palm.tree  = palm.tree$tip.label)
+species <- list(palm.dist  = levels(palm.dist$SpecName),
+                trait.data = sub(pattern=" ", replacement="_",
+                                 x=trait.data$SpecName), 
+                palm.tree  = palm.tree$tip.label)
 # 2. cross-check
 missing.table <- multiCC(species, presence=FALSE, value=FALSE)
 missing.list <- multiCC(species, presence=FALSE, value=TRUE)
@@ -49,38 +51,52 @@ missing.list <- multiCC(species, presence=FALSE, value=TRUE)
 # 3. Find the list of species upon which all three sources agree
 present.list <- multiCC(species, presence=TRUE, value=TRUE)
 # Just to verify:
-new.species <- list(
-  palm.dist = present.list$palm.dist$palm.tree,
-  palm.tree = present.list$palm.tree$palm.dist)
+new.species <- list(palm.dist = present.list$palm.dist$palm.tree,
+                    palm.tree = present.list$palm.tree$palm.dist)
 multiCC(new.species, presence=FALSE, value=FALSE)
 # Yup, nothing missing.
 species.agreed <- sort(present.list$palm.dist$palm.tree)
 
 
 # Subset palm traits dataset
-palm.traits    <- data.frame(
-  species      = sub(pattern=" ", replacement="_", x=trait.data$SpecName),
-  genus        = trait.data$accGenus,
-  stem.height  = trait.data$MaxStemHeight_m,  # maximum stem height in m.
-  blade.length = trait.data$Max_Blade_Length_m,  # maximum blade length in m.
-  fruit.length = trait.data$AverageFruitLength_cm)  # Average fruit length in cm.
+palm.traits <- data.frame(species      = sub(pattern=" ", replacement="_",
+                                             x=trait.data$SpecName),
+                          genus        = trait.data$accGenus,
+                          stem.height  = trait.data$MaxStemHeight_m,
+                          blade.length = trait.data$Max_Blade_Length_m,
+                          fruit.length = trait.data$AverageFruitLength_cm)
+# stem.height = maximum stem height in meters.
+# blade.length = maximum blade length in meters.
+# fruit.length = average fruit length in centimeters.
 palm.traits <- palm.traits[crossCheck(palm.traits$species,
-  species.agreed, presence=TRUE, value=FALSE), ]
-write.csv(palm.traits, file="output/palm.traits.subset.csv", eol="\r\n")
+                                      species.agreed, presence=TRUE,
+                                      value=FALSE), ]
+write.csv(palm.traits, file="output/palm.traits.csv", eol="\r\n")
+
 
 # Calculate Genus-level mean trait values and fill trait matrix
 genus.means <- ddply(palm.traits, "genus", numcolwise(mean, na.rm=TRUE))
-# Beware NAs for genera where all species have NA for the same trait
-# Those genera and all their species may have to be deleted
-# That is a drawback BHPMF *may* not have
-# TODO: removing these species / genera
 traits.filled.means <- gapFill(palm.traits, genus.means, by="genus",
-  fill=c("stem.height", "blade.length", "fruit.length"))
+                               fill=c("stem.height", "blade.length",
+                                      "fruit.length"))
+# Beware NAs for genus means where all species of the genushave NA for the
+# same trait.
+# Solution: delete all cases where genus-mean filling is not possible
+# That is a drawback BHPMF *may* not have.
+# TODO: discuss this solution
+genus.mean.missing <- traits.filled.means[!complete.cases(traits.filled.means), ]
+traits.filled.means <- traits.filled.means[complete.cases(traits.filled.means), ]
+write.csv(traits.filled.means, file="output/palm.traits.genus.mean.csv",
+          eol="\r\n")
+write.csv(genus.mean.missing, file="output/genus.mean.missing.csv", eol="\r\n")
 
-# TODO: finish gap-filling with genus means
+
+
+
+
 
 # TODO: Subset all datasets to the list of agreed species.
-# Can we remove species from the phylogenetic tree? How?
+#   Can we remove species from the phylogenetic tree? How?
 
 
 
