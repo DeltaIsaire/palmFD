97392b5a548d83add9941a700adb2a9bf683723f
DeltaIsaire <management.organisatie@gmail.com>
2019-02-09 15:21:01 +0100

    Written faster version of FD::dbFD, tailored to our needs

---
+++ functions/faster_FD_function.R
@@ -0,0 +1,159 @@
+#########################################
+# faster, streamlined version of FD::dbFD
+#########################################
+#
+# The original function dbFD() from package 'FD' does a lot of things we do not
+# need it to do. Runtime is a constraint for us, hence it is worthwile to rewrite
+# this function to do only what it strictly needs to for our purposes.
+
+
+library(FD)
+
+
+test <- FALSE
+
+# Extract original source code
+# ----------------------------
+if (test) {
+  capture.output(getAnywhere("dbFD"), file = "functions/source_dbFD.R")
+}
+
+# Input data to test with
+# -----------------------
+if (test) {
+  trait.matrix <- data.frame(a.trait = 1:10,
+                             b.trait = 15:6,
+                             c.trait = c(7, 8, 9, 8, 6, 4, 9, 6, 7, 5),
+                             row.names = paste0("species.", letters[1:10])
+                             )
+  trait.matrix <- as.matrix(trait.matrix)
+
+  set.seed(123)
+  pres.abs.matrix <- matrix(ncol = nrow(trait.matrix),
+                            nrow = 10,
+                            dimnames = list(row = paste0("area.", letters[1:10]),
+                                            col = rownames(trait.matrix)
+                                            ),
+                            data = round(runif(10 * nrow(trait.matrix)))
+                            )
+  pres.abs.matrix[6, c(1, 6)] <- 1
+  pres.abs.matrix[10, c(1, 6)] <- 1
+}
+
+# Function rewrite
+# ----------------
+# We only need two outputs: FRic and FDis
+
+FastFD <- function(trait.matrix, pres.abs.matrix) {
+  # No checks. Assume input is valid.
+  # Trait.matrix: a matrix with trait values in columns, and rownames as species.
+  # pres.abs.matrix: a matrix with presence (1) and absense (0) of species
+  #                  (colnames) in tdwg3 units (rownames).
+  # nrow(trait.matrix) must equal ncol(pres.abs.matrix)
+  # rownames(trait.matrix) must equal colnames(pres.abs.matrix)
+  # Neither matrix should have NAs.
+  # No community should have zero species, and all species should occur somewhere.
+  # There shall be exactly 3 traits, all numeric.
+  # All traits will be standardized to mean 0 and unit variance.
+  # All communities must have > 3 species with unique trait combinations
+  x <- trait.matrix
+  a <- pres.abs.matrix
+
+  s.x <- dim(x)[1]
+  t.x <- dim(x)[2]
+  x.rn <- row.names(x)
+
+  c <- dim(a)[1]
+  s.a <- dim(a)[2]
+  ab.t <- t(a)
+  ab.t.row <- row.names(ab.t)
+
+  abun.sum <- rowSums(a)
+  abun.sum2 <- colSums(a)
+
+  w <- rep(1, t.x) / sum(rep(1, t.x))
+
+  x <- data.frame(x)
+  x.s <- apply(x, 2, scale, center = TRUE, scale = TRUE)
+  x.dist <- dist(x.s)
+  attr(x.dist, "Labels") <- x.rn
+  x.dist2 <- x.dist
+
+  x.pco <- dudi.pco(x.dist2, scannf = FALSE, full = TRUE)
+  traits <- round(x.pco$li, .Machine$double.exponent)
+
+  if (x.pco$nf == 1) {
+    traits.FRic <- x.pco$li
+    cat("FRic: Only one continuous trait or dimension in 'x'. FRic was measured as the range, NOT as the convex hull volume.\n")
+  }
+  if (x.pco$nf > 1) {
+    traits.FRic <- x.pco$li
+  }
+  
+  disp <- fdisp(x.dist, a)
+  FDis <- disp$FDis
+  
+  FRic <- rep(NA, c)
+  names(FRic) <- row.names(a)
+  
+  for (i in seq_len(c)) {
+    sppres <- which(a[i, ] > 0)
+    S <- length(sppres)
+    tr <- data.frame(traits[sppres, ])
+    tr.FRic <- data.frame(traits.FRic[sppres, ])
+    ab <- as.matrix(a[i, sppres])
+    abundrel <- ab / sum(ab)
+    if (dim(tr.FRic)[2] > 1) {
+      thresh <- 3
+      convhull <- convhulln(tr.FRic, "FA")
+      FRic[i] <- convhull$vol
+    }
+    if (dim(tr.FRic)[2] == 1) {
+      tr.range <- range(tr.FRic[, 1])
+      t.range <- tr.range[2] - tr.range[1]
+      FRic[i] <- t.range
+    }
+  }
+
+  res <- vector("list", length = 2)
+  names(res) <- c("FRic", "FDis")
+  res$FRic <- FRic
+  res$FDis <- FDis
+
+  invisible(res)
+}
+
+
+# Test new function: compare to original
+# --------------------------------------
+if (test) {
+  system.time(new <- FastFD(trait.matrix, pres.abs.matrix))
+  #   user  system elapsed 
+  #  0.020   0.000   0.018 
+
+  system.time( {
+    old <- dbFD(x = trait.matrix,
+                a = pres.abs.matrix,
+                w.abun = FALSE,
+                stand.x = TRUE,
+                corr = "cailliez",
+                calc.FRic = TRUE,
+                m = "max",
+                stand.FRic = FALSE,
+                calc.CWM = FALSE,
+                calc.FDiv = FALSE
+                )
+    old <- old[c("FRic", "FDis")]
+  }
+  )
+  #   user  system elapsed 
+  #  0.048   0.004   0.052 
+
+  identical(new, old)
+  # TRUE
+
+  # Evaluation:
+  # For this test dataset, the new function works as expected,
+  # and is significantly faster. Mission Accomplished.
+}
+
